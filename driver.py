edfda6ca22ec1e65f92262ac08a99755a5054321
keyapi <43301887+keyapi@users.noreply.github.com>
2020-03-29 11:43:17 +0200

    Update driver.py

--- driver.py
+++ driver.py
@@ -31,8 +31,6 @@ def driver_test():
     xpath_google_img = "//img[@alt='Google']"
     elem_google_img = driver.find_element_by_xpath(xpath_google_img)
     print(elem_google_img.get_attribute('src')
-
-
     driver.quit()
 
 if __name__ == "__main__":
